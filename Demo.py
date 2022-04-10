177d39fb2752158df09f77e82e44814a7d0ce407
vijaykhalate <vijaykhalate05@gmail.com>
2022-04-10 15:41:20 +0530

    Two line Added

--- Demo.py
+++ Demo.py
@@ -8,3 +8,6 @@ a,b=b,a
 
 print("Value of a after swapping : ",a)
 print("Value of b after swapping : ",b)
+
+print("This file is Imported from GitHub that is Pull")
+print("Modify this file and send it to GitHub that is Push")
