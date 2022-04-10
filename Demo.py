0fc170e8301fd0c2e2bc6e9d376fc425c3505d29
vijaykhalate <102990059+vijaykhalate@users.noreply.github.com>
2022-04-10 15:29:08 +0530

    Create Demo.py

---
+++ Demo.py
@@ -0,0 +1,10 @@
+a = 10
+b = 20
+
+print("Value of a before swapping : ",a)
+print("Value of b before swapping : ",b)
+
+a,b=b,a
+
+print("Value of a after swapping : ",a)
+print("Value of b after swapping : ",b)
